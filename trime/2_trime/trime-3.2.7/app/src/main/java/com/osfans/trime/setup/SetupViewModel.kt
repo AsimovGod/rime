a9f2e059eae13210b88a63dbf7938b209bcbabf0
AsimovGod <AsimovGod@outlook.com>
2024-11-24 18:13:21 +0800

    clear

--- trime/2_trime/trime-3.2.7/app/src/main/java/com/osfans/trime/setup/SetupViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.osfans.trime.setup
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class SetupViewModel : ViewModel() {
-    val isAllDone = MutableLiveData(false)
-}
--- trime/2_trime/trime-3.2.8/app/src/main/java/com/osfans/trime/util/Const.kt
+++
@@ -1,7 +0,0 @@
-package com.osfans.trime.util
-
-import com.osfans.trime.BuildConfig
-
-object Const {
-    const val buildGitHash = BuildConfig.BUILD_GIT_HASH
-}
